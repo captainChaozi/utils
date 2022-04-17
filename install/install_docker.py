1cbbf5d824b1a044d7b8f69db7a5f814ceae3803
alan.zhang <alan.zhang@likade.com>
2022-04-17 23:24:50 +0800

    <fix> 修复docker 镜像加速BUG

--- install/install_docker.py
+++ install/install_docker.py
@@ -4,16 +4,11 @@ import os
 def install_docker():
     step1 = os.system("wget -qO- https://get.docker.com/ | sh")
     assert step1 == 0, "docker 安装失败"
-    step2 = os.system('sudo usermod -aG docker chao')
+    step2 = os.system('sudo usermod -aG docker $USER')
     assert step2 == 0, "docker 用户组更改失败"
     os.system("sudo mkdir -p /etc/docker")
-    step3 = os.system("""
-    sudo tee /etc/docker/daemon.json <<-'EOF'
-    {
-    "registry-mirrors": ["https://m6wlkecl.mirror.aliyuncs.com"]
-    }
-    EOF""")
-    assert step3 == 0, "docker 镜像地址更新失败"
+    with open('/etc/docker/daemon.json','rw') as f:
+        f.write('{"registry-mirrors": ["https://m6wlkecl.mirror.aliyuncs.com"]}')
     os.system("sudo systemctl daemon-reload")
     os.system("sudo systemctl restart docker")
     print("docker安装成功")
